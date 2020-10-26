112775b5286d542edbc4d833dd42595d2225016e
Praatibh Surana <praatibhsurana@gmail.com>
2020-10-26 16:10:31 +0530

    Made basic SVM Classifier

---
+++ model.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn import svm
+from sklearn import metrics
+import preprocessing as pre
+
+
+# Accessing dataset
+data = pd.read_csv("data.csv")
+# print(data.head())
+print("Initial shape:", data.shape)
+data = pd.DataFrame(data)
+
+data, labels = pre.preprocess(data)
+print(
+    "Shape after preprocessing:", data.shape, "and length of target array:", len(labels)
+)
+
+train, train_labels, test, test_labels = pre.train_test_split(data, labels)
+print("Train data:", train.shape, len(train_labels))
+print("Test data:", test.shape, len(test_labels))
+
+# SVM Classifier
+clf = svm.SVC(kernel="rbf", C=1.0)  # rbf kernel
+
+# Train the model using the training sets
+clf.fit(train, train_labels)
+
+# Predict the response for test dataset
+y_pred = clf.predict(test)
+
+print("Accuracy:", metrics.accuracy_score(test_labels, y_pred))
+
+prec = metrics.precision_score(test_labels, y_pred)
+# Model Precision: what percentage of positive tuples are labeled as such?
+print("Precision:", prec)
+
+rec = metrics.recall_score(test_labels, y_pred)
+# Model Recall: what percentage of positive tuples are labelled as such?
+print("Recall:", rec)
+
+print("F-1 Score:", (2 * prec * rec) / (prec + rec))
---
+++ preprocessing.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+# import seaborn as sns
+import sklearn
+
+
+def preprocess(data):
+    # Annotating labels
+    labels = []
+    for i in range(len(data["diagnosis"])):
+        if data["diagnosis"][i] == "M":
+            labels.append(1)
+        else:
+            labels.append(0)
+
+    # dropping non impacting columns as observed from heatmap
+    data = data.drop(
+        [
+            "symmetry_se",
+            "fractal_dimension_se",
+            "smoothness_se",
+            "fractal_dimension_mean",
+            "diagnosis",
+            "id",
+            "Unnamed: 32",
+        ],
+        axis=1,
+    )
+
+    return data, labels
+
+
+def train_test_split(data, labels):
+    data = data.to_numpy()
+
+    # Train and test data
+    train = data[:450]
+    train_labels = np.array(labels[:450])
+    test = data[450:]
+    test_labels = np.array(labels[450:])
+
+    # Normalizing data
+    train = sklearn.preprocessing.normalize(
+        train, axis=1, norm="l2", copy=False, return_norm=False
+    )
+    test = sklearn.preprocessing.normalize(
+        test, axis=1, norm="l2", copy=False, return_norm=False
+    )
+
+    return train, train_labels, test, test_labels
